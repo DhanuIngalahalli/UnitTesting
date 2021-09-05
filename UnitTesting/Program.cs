d9e29648b2b48f72e0343ee6e75281e34f124297
sonupooja <ingalahallipooja@gmail.com>
2021-09-05 23:58:52 +0530

    adding vending machine program

--- UnitTesting/Program.cs
+++ UnitTesting/Program.cs
@@ -4,9 +4,19 @@ namespace UnitTesting
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public class VendingMachine
         {
-            Console.WriteLine("unit testing");
+           
+            public VendingMachine()
+            {
+                Console.WriteLine("Enter money amount:");
+                int money = Convert.ToInt32(Console.ReadLine());
+                int[] notes = new int[] { 1, 2, 5, 10, 50, 500, 1000 };
+                int length = notes.Length;
+                Utility utility = new Utility();
+                Console.WriteLine("Minimum Notes= " + utility.MinNotes(notes, length, money));
+            }
         }
     }
 }
